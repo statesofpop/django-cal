ae76741e10515e45646f0c80b23e5bc6bbb10278
Gabriel Grant <g@briel.ca>
2011-02-05 05:08:22 -0500

    Added an AUTHORS file and a setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='django-cal',
+    version='0.1dev',
+    author='Maik Hoepfel',
+    packages=['django_cal',],
+    license='BSD',
+    long_description=open('README').read(),
+    install_requires=[
+        'vobject',
+    ],
+)
