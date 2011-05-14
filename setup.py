5aa596dd4e8cb7ddb1cfa730546a432475b431a0
Gabriel Grant <g@briel.ca>
2011-05-14 14:19:49 -0700

    add missing comma in requirements list

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     license='BSD',
     long_description=open('README.md').read(),
     install_requires=[
-        'python-dateutil < 2.0'  # dateutil (used by vobject) 2+ only works with Python 3
+        'python-dateutil < 2.0',  # dateutil (used by vobject) 2+ only works with Python 3
         'vobject',
     ],
 )
