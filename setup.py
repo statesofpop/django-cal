9b291e6afa8982a906c5c979c8b240018171fa3f
Maik Hoepfel <m@maikhoepfel.de>
2013-01-09 11:35:22 +0000

    Get package into PyPi.

--- setup.py
+++ setup.py
@@ -10,7 +10,6 @@ setup(
     keywords='Django iCalendar iCal ics vobject',
     url = 'http://github.com/statesofpop/django-cal',
     license='BSD',
-    long_description=open('README.md').read(),
     install_requires=[
         'python-dateutil < 2.0',  # dateutil (used by vobject) 2+ only works with Python 3
         'vobject',
